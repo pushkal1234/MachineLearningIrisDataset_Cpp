30f2de3057c2b553e497e141559b36dbb8c5f11d
Pushkal Shukla <pushkalshuk@gmail.com>
2021-04-16 22:56:59 +0530

    Add files via upload

---
+++ Layer2/cmake-build-debug/CMakeFiles/Layer2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Layer2.dir/main.cpp.obj"
+  "Layer2.exe"
+  "Layer2.exe.manifest"
+  "Layer2.pdb"
+  "libLayer2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Layer2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
